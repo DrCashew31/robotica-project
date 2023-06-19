264e2fac99ddccbf1b5d24d94bb2ff96a71677a9
DrCashew31 <113105239+DrCashew31@users.noreply.github.com>
2023-06-19 12:18:30 +0300

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,24 +1,37 @@
 radio.onReceivedNumber(function (receivedNumber) {
     if (receivedNumber == 1) {
-        pins.digitalWritePin(DigitalPin.P13, 0)
-        pins.digitalWritePin(DigitalPin.P12, 1)
-        basic.pause(5000)
-        pins.digitalWritePin(DigitalPin.P12, 0)
-        pins.digitalWritePin(DigitalPin.P13, 0)
-    } else if (receivedNumber == 0) {
-        pins.digitalWritePin(DigitalPin.P12, 0)
-        pins.digitalWritePin(DigitalPin.P12, 0)
+        COUNT = 1
+    }
+})
+let COUNT = 0
+let UPDOWN = 0
+COUNT = 0
+radio.setGroup(10)
+basic.forever(function () {
+    if (COUNT == 1 && UPDOWN == 0) {
+        pins.analogWritePin(AnalogPin.P13, 0)
+        pins.analogWritePin(AnalogPin.P12, 701)
+        basic.pause(3750)
+        pins.analogWritePin(AnalogPin.P13, 0)
+        pins.analogWritePin(AnalogPin.P12, 0)
+        UPDOWN = 1
+    } else if (COUNT == 0 && UPDOWN == 1) {
+        pins.analogWritePin(AnalogPin.P12, 0)
+        pins.analogWritePin(AnalogPin.P13, 701)
+        basic.pause(3750)
+        pins.analogWritePin(AnalogPin.P12, 0)
+        pins.analogWritePin(AnalogPin.P13, 0)
+        UPDOWN = 0
     }
 })
 basic.forever(function () {
-    radio.setGroup(1)
+    TM1650.on()
+    TM1650.showNumber(ModuleWorld_Analog.Light(ModuleWorld_Analog.mwAnalogNum.AP2P3))
 })
 basic.forever(function () {
-    if (ModuleWorld_Analog.Light(ModuleWorld_Analog.mwAnalogNum.AP2P3) > 970) {
+    if (ModuleWorld_Analog.Light(ModuleWorld_Analog.mwAnalogNum.AP2P3) > 980) {
         radio.sendNumber(1)
-    } else if (ModuleWorld_Analog.Light(ModuleWorld_Analog.mwAnalogNum.AP2P3) < 970) {
+    } else if (ModuleWorld_Analog.Light(ModuleWorld_Analog.mwAnalogNum.AP2P3) < 800) {
         radio.sendNumber(0)
-    } else {
-    	
     }
 })
