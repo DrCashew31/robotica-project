461e6b7efd86840114a3cde38749f8e7a3184e3f
DrCashew31 <113105239+DrCashew31@users.noreply.github.com>
2023-06-19 14:46:36 +0300

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,10 @@
 radio.onReceivedNumber(function (receivedNumber) {
     if (receivedNumber == 1) {
         COUNT = 1
+        basic.showNumber(1)
+    } else if (receivedNumber == 0) {
+        COUNT = 0
+        basic.showNumber(0)
     }
 })
 let COUNT = 0
@@ -10,15 +14,15 @@ radio.setGroup(10)
 basic.forever(function () {
     if (COUNT == 1 && UPDOWN == 0) {
         pins.analogWritePin(AnalogPin.P13, 0)
-        pins.analogWritePin(AnalogPin.P12, 701)
-        basic.pause(3750)
+        pins.analogWritePin(AnalogPin.P12, 900)
+        basic.pause(6800)
         pins.analogWritePin(AnalogPin.P13, 0)
         pins.analogWritePin(AnalogPin.P12, 0)
         UPDOWN = 1
     } else if (COUNT == 0 && UPDOWN == 1) {
         pins.analogWritePin(AnalogPin.P12, 0)
-        pins.analogWritePin(AnalogPin.P13, 701)
-        basic.pause(3750)
+        pins.analogWritePin(AnalogPin.P13, 900)
+        basic.pause(6800)
         pins.analogWritePin(AnalogPin.P12, 0)
         pins.analogWritePin(AnalogPin.P13, 0)
         UPDOWN = 0
@@ -31,7 +35,9 @@ basic.forever(function () {
 basic.forever(function () {
     if (ModuleWorld_Analog.Light(ModuleWorld_Analog.mwAnalogNum.AP2P3) > 980) {
         radio.sendNumber(1)
+        basic.showNumber(1)
     } else if (ModuleWorld_Analog.Light(ModuleWorld_Analog.mwAnalogNum.AP2P3) < 800) {
         radio.sendNumber(0)
+        basic.showNumber(0)
     }
 })
