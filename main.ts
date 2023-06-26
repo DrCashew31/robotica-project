ab2233c8bbe66178ef030a010398a7ddb316aa94
DrCashew31 <113105239+DrCashew31@users.noreply.github.com>
2023-06-26 23:10:57 +0300

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -14,15 +14,15 @@ radio.setGroup(10)
 basic.forever(function () {
     if (COUNT == 1 && UPDOWN == 0) {
         pins.analogWritePin(AnalogPin.P12, 0)
-        pins.analogWritePin(AnalogPin.P13, 840)
+        pins.analogWritePin(AnalogPin.P13, 750)
         basic.pause(4800)
         pins.analogWritePin(AnalogPin.P13, 0)
         pins.analogWritePin(AnalogPin.P12, 0)
         UPDOWN = 1
     } else if (COUNT == 0 && UPDOWN == 1) {
         pins.analogWritePin(AnalogPin.P13, 0)
-        pins.analogWritePin(AnalogPin.P12, 750)
-        basic.pause(4300)
+        pins.analogWritePin(AnalogPin.P12, 840)
+        basic.pause(4350)
         pins.analogWritePin(AnalogPin.P12, 0)
         pins.analogWritePin(AnalogPin.P13, 0)
         UPDOWN = 0
@@ -32,7 +32,7 @@ basic.forever(function () {
     if (ModuleWorld_Analog.Light(ModuleWorld_Analog.mwAnalogNum.AP2P3) > 980) {
         radio.sendNumber(1)
         basic.showNumber(1)
-    } else if (ModuleWorld_Analog.Light(ModuleWorld_Analog.mwAnalogNum.AP2P3) < 800) {
+    } else if (ModuleWorld_Analog.Light(ModuleWorld_Analog.mwAnalogNum.AP2P3) < 880) {
         radio.sendNumber(0)
         basic.showNumber(0)
     }
